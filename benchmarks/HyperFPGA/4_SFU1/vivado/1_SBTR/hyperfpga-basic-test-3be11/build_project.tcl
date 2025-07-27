36683779519cda2f32cbb01a6a87848d7ace8ed7
divadnauj-GB <juan.guerrero@polito.it>
2025-07-27 21:00:20 +0200

    vivado build project script added

---
+++ benchmarks/HyperFPGA/4_SFU1/vivado/1_SBTR/hyperfpga-basic-test-3be11/build_project.tcl
@@ -0,0 +1,20 @@
+# Open the Vivado project
+
+open_project ./basic_test-3be11_prj/basic_test-3be11_prj.xpr
+
+# Launch synthesis run
+launch_runs synth_1
+wait_on_run synth_1
+
+# Launch implementation run and generate bitstream
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
+
+# Optional: Generate reports or perform other actions
+#report_timing_summary
+#report_utilization
+
+write_hw_platform -fixed -include_bit -force -file SFU1_1_SBTR-3be11.xsa
+
+close_project
+exit
