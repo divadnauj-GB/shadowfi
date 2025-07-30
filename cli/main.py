46f2a1f8bf2609b6ed67a2367aeda3b4d7a8f5d9
divadnauj-GB <juan.guerrero@polito.it>
2025-07-30 10:57:58 +0200

    updated args

--- cli/main.py
+++ cli/main.py
@@ -42,19 +42,19 @@ def cli_entry(current_project=None):
     fsim_setup_parser.add_argument('--kwargs', nargs='*', action=KeyValueAction, help="Nested key-value pairs, e.g. a.b.c=val")
     fsim_setup_parser.add_argument('--run-script', default=None, help='Path to the testbench configuration file')
     fsim_setup_parser.add_argument('--sdc-check-script', default=None, help='Path to the testbench configuration file')
-    fsim_setup_parser.add_argument('--noset-run-scripts', default=False, action=argparse.BooleanOptionalAction, help= "enable setting any run.sh and sdc_check.sh scripts, otherwise directly provided by the user")
+    fsim_setup_parser.add_argument('--noset-run-scripts', action="store_true", help= "enable setting any run.sh and sdc_check.sh scripts, otherwise directly provided by the user")
 
     fsim_exec_parser = subparsers.add_parser('fsim_exec')
     fsim_exec_parser.add_argument('--fsim-config', default=None, help='Path to the testbench configuration file')
     fsim_exec_parser.add_argument('--kwargs', nargs='*', action=KeyValueAction, help="Nested key-value pairs, e.g. a.b.c=val")
-    fsim_exec_parser.add_argument('--hpc',default=False, action=argparse.BooleanOptionalAction, help="enable HPC execution")
+    fsim_exec_parser.add_argument('--hpc',action="store_true", help="enable HPC execution")
     #fsim_exec_parser.add_argument('--work-dir-root',default=None,  help="optionalworking directory root, useful when hpc job is executed")
     #fsim_exec_parser.add_argument('--slurm-jobid',default="./",  help="optionalworking directory root, useful when hpc job is executed")
 
 
     fi_fpga_setup_parser = subparsers.add_parser('fi_fpga_setup')
     fi_fpga_setup_parser.add_argument('--emu-config', default=None, help='Path to the configuration file')
-    fi_fpga_setup_parser.add_argument('--no-compile-vivado', default=False, action=argparse.BooleanOptionalAction, help='enable vivado compilation')
+    fi_fpga_setup_parser.add_argument('--no-compile-vivado', action="store_true", help='enable vivado compilation')
     fi_fpga_setup_parser.add_argument('--kwargs', nargs='*', action=KeyValueAction, help="Nested key-value pairs, e.g. a.b.c=val")
 
 
